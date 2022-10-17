fc4fab3b1eaf2ef9c7a49092a16554f9599bb939
dmtrack <rightrack888@gmail.com>
2022-10-17 21:31:15 +0600

    add: eslint rule

--- src/components/Alert.jsx
+++ src/components/Alert.jsx
@@ -8,6 +8,7 @@ function Alert(props) {
     return () => {
       clearTimeout(timerId);
     };
+    // eslint-disable-next-line
   }, [name]);
   return (
     <div id="toast-container">
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,7 @@ function Header() {
   return (
     <nav className="#eceff1 blue-grey lighten-5 navbar-fixed">
       <div className="nav-wrapper">
-        <a href="#" className="brand-logo" style={{ color: "#4db6ac" }}>
+        <a href="!#" className="brand-logo" style={{ color: "#4db6ac" }}>
           e-commerce
         </a>
         <ul id="nav-mobile" className="right hide-on-med-and-down">
